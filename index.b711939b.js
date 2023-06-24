cd4fd4f71264f6d8ef27fa915ac86ebe25947a8a
RomaSamkov <92638707+RomaSamkov@users.noreply.github.com>
2023-06-24 14:47:12 +0000

    Deploying to gh-pages from @ RomaSamkov/digital-clock@ac6233b0643e0f839882eea779aec11d07b15184 🚀

--- index.b711939b.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelector("#hr"),t=document.querySelector("#mn"),r=document.querySelector("#sc");setInterval(()=>{let o=new Date,n=30*o.getHours(),s=6*o.getMinutes(),l=6*o.getSeconds();e.style.transform=`rotateZ(${n+s/12}deg)`,t.style.transform=`rotateZ(${s}deg)`,r.style.transform=`rotateZ(${l}deg)`});
-//# sourceMappingURL=index.b711939b.js.map
